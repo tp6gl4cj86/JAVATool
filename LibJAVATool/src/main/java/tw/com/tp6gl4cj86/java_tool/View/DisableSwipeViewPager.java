7bbb013ef9e50626818136011eebeb2ae00a6717
tp6gl4cj86 <tp6gl4cj86@gmail.com>
2017-09-06 15:50:21 +0800

    1.0.8，update butterknife 8.8.1

---
+++ LibJAVATool/src/main/java/tw/com/tp6gl4cj86/java_tool/View/DisableSwipeViewPager.java
@@ -0,0 +1,38 @@
+package tw.com.tp6gl4cj86.java_tool.View;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by tp6gl4cj86 on 15/3/6.
+ */
+public class DisableSwipeViewPager extends ViewPager
+{
+
+    private boolean isEnableSwipe = false;
+
+    public DisableSwipeViewPager(Context context)
+    {
+        super(context);
+    }
+
+    public DisableSwipeViewPager(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event)
+    {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return false;
+    }
+
+}
