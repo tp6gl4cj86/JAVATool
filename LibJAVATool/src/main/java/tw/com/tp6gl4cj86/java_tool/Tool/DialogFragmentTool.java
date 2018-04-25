d6700004d809b37efab11af9016a6f05d631e9df
tp6gl4cj86 <tp6gl4cj86@gmail.com>
2018-04-25 18:35:39 +0800

    no message

--- LibJAVATool/src/main/java/tw/com/tp6gl4cj86/java_tool/Tool/DialogFragmentTool.java
+++ LibJAVATool/src/main/java/tw/com/tp6gl4cj86/java_tool/Tool/DialogFragmentTool.java
@@ -1,5 +1,6 @@
 package tw.com.tp6gl4cj86.java_tool.Tool;
 
+
 import android.app.DialogFragment;
 import android.os.Build;
 import android.view.View;
@@ -29,23 +30,32 @@ public class DialogFragmentTool
      * with status bar
      * onCreate
      */
-    public static void setFullScreen(DialogFragment dialog)
+    public static <DF> void setFullScreen(DF dialog)
     {
-        if (dialog != null)
-        {
-            dialog.setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialog);
-        }
+        setWindowStyle(dialog, R.style.FullScreenDialog);
     }
 
     /**
      * without status bar
      * onCreate
      */
-    public static void setFullWindowScreen(DialogFragment dialog)
+    public static <DF> void setFullWindowScreen(DF dialog)
+    {
+        setWindowStyle(dialog, R.style.FullWindowScreenDialog);
+    }
+
+    private static <DF> void setWindowStyle(DF dialog, int style)
     {
         if (dialog != null)
         {
-            dialog.setStyle(DialogFragment.STYLE_NORMAL, R.style.FullWindowScreenDialog);
+            if (dialog instanceof DialogFragment)
+            {
+                ((DialogFragment) dialog).setStyle(DialogFragment.STYLE_NORMAL, style);
+            }
+            else if (dialog instanceof android.support.v4.app.DialogFragment)
+            {
+                ((android.support.v4.app.DialogFragment) dialog).setStyle(android.support.v4.app.DialogFragment.STYLE_NORMAL, style);
+            }
         }
     }
 
@@ -63,15 +73,26 @@ public class DialogFragmentTool
     /**
      * onCreateView
      */
-    public static void setStatusBarColor(DialogFragment dialog, int color)
+    public static <DF> void setStatusBarColor(DF dialog, int color)
     {
-        setStatusBarColor(dialog.getDialog()
-                                .getWindow(), color);
+        if (dialog != null)
+        {
+            if (dialog instanceof DialogFragment)
+            {
+                setStatusBarColor(((DialogFragment) dialog).getDialog()
+                                                           .getWindow(), color);
+            }
+            else
+            {
+                setStatusBarColor(((android.support.v4.app.DialogFragment) dialog).getDialog()
+                                                                                  .getWindow(), color);
+            }
+        }
     }
 
     public static void setStatusBarColor(Window window, int color)
     {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        if (window != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
         {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -82,31 +103,45 @@ public class DialogFragmentTool
     /**
      * onCreateView
      */
-    public static void setBehindStatusBar(DialogFragment dialog)
+    public static <DF> void setBehindStatusBar(DF dialog)
     {
         setBehindStatusBar(dialog, 0x00000000);
     }
 
-    public static void setBehindStatusBar(DialogFragment dialog, int color)
+    public static <DF> void setBehindStatusBar(DF dialog, int color)
     {
-        setBehindStatusBar(dialog.getDialog()
-                                 .getWindow(), color);
+        if (dialog != null)
+        {
+            if (dialog instanceof DialogFragment)
+            {
+                setBehindStatusBar(((DialogFragment) dialog).getDialog()
+                                                            .getWindow(), color);
+            }
+            else if (dialog instanceof android.support.v4.app.DialogFragment)
+            {
+                setBehindStatusBar(((android.support.v4.app.DialogFragment) dialog).getDialog()
+                                                                                   .getWindow(), color);
+            }
+        }
     }
 
     public static void setBehindStatusBar(Window window, int color)
     {
-        if (Build.VERSION.SDK_INT >= 16)
+        if (window != null)
         {
-            window.getDecorView()
-                  .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-        {
-            window.setStatusBarColor(color);
-        }
-        else if (Build.VERSION.SDK_INT >= 19)
-        {
-            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            if (Build.VERSION.SDK_INT >= 16)
+            {
+                window.getDecorView()
+                      .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            {
+                window.setStatusBarColor(color);
+            }
+            else if (Build.VERSION.SDK_INT >= 19)
+            {
+                window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            }
         }
     }
 
@@ -124,21 +159,36 @@ public class DialogFragmentTool
     /**
      * onCreateView
      */
-    public static void setKeyBoardResize(DialogFragment dialog)
+    public static <DF> void setKeyBoardResize(DF dialog)
     {
-        dialog.getDialog()
-              .getWindow()
-              .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        setKeyBoardStyle(dialog, WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
     }
 
     /**
      * onCreateView
      */
-    public static void setKeyBoardPan(DialogFragment dialog)
+    public static <DF> void setKeyBoardPan(DF dialog)
     {
-        dialog.getDialog()
-              .getWindow()
-              .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+        setKeyBoardStyle(dialog, WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+    }
+
+    private static <DF> void setKeyBoardStyle(DF dialog, int style)
+    {
+        if (dialog != null)
+        {
+            if (dialog instanceof DialogFragment)
+            {
+                ((DialogFragment) dialog).getDialog()
+                                         .getWindow()
+                                         .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            }
+            else if (dialog instanceof android.support.v4.app.DialogFragment)
+            {
+                ((android.support.v4.app.DialogFragment) dialog).getDialog()
+                                                                .getWindow()
+                                                                .setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+            }
+        }
     }
 
 }
