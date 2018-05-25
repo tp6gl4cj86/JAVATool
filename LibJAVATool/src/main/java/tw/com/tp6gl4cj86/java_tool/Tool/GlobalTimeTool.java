0b0567899de7b96f323e312e4c100e59c6f25dd1
tp6gl4cj86 <tp6gl4cj86@gmail.com>
2018-05-25 18:41:15 +0800

    3.1.0

--- LibJAVATool/src/main/java/tw/com/tp6gl4cj86/java_tool/Tool/GlobalTimeTool.java
+++ LibJAVATool/src/main/java/tw/com/tp6gl4cj86/java_tool/Tool/GlobalTimeTool.java
@@ -10,9 +10,15 @@ import java.util.TimeZone;
  */
 public class GlobalTimeTool
 {
-    private static SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     private static String timeZone = "GMT+8:00";
+
+    public static SimpleDateFormat getDateFormat()
+    {
+        mSimpleDateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
+        return mSimpleDateFormat;
+    }
 
     /**
      * @param TimeZone ex : "GMT+8:00"
@@ -44,6 +50,24 @@ public class GlobalTimeTool
         catch (ParseException e)
         {
             e.printStackTrace();
+            date = new Date();
+        }
+        return date;
+    }
+
+    public static Date getGlobalTimeDate(String dateStr)
+    {
+        mSimpleDateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        Date date = null;
+        try
+        {
+            date = mSimpleDateFormat.parse(dateStr);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+            date = new Date();
         }
         return date;
     }
